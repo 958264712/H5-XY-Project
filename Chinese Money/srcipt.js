7672a2d6f444591bae4a4a2520a001e65ef8d56b
blithe <958264712@qq.com>
2023-12-15 14:46:03 +0800

    添加新项目

---
+++ Chinese Money/srcipt.js
@@ -0,0 +1,13 @@
+
+// var tabArr = $('.item');
+// for (var i = 0; i < tabArr.length; i++) {
+//     $(tabArr[i]).bind("click", { index: i }, clickHandler);
+// }
+// function clickHandler(event) {
+// 	console.log(event);
+// 	console.log(document.documentElement.scrollTop)
+// 	console.log($('#pop-up').prop("height")/2)
+// 	$('#pop-up').css("display","block");
+// 	$('#pop-up').css ('top',(document.documentElement.scrollTop + $('#pop-up').prop("height")/2)) 
+	
+// }
